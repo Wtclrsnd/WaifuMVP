82f268a6ed7e384d37b7e3bd3d99236ee1a8fa10
Emil Shpeklord <emil42661@gmail.com>
2022-04-19 15:21:29 +0300

    done

--- mvpTest/WaifuViewController.swift
+++ mvpTest/WaifuViewController.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 
-final class WaifuViewController: UIViewController {
+final class WaifuViewController: UIViewController, WaifuProtocol {
+
+    private var presenter = WaifuPresenter(service: WaifuApiService())
+
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .clear
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -18,6 +23,8 @@ final class WaifuViewController: UIViewController {
         let button = UIButton()
         button.backgroundColor = .systemPink
         button.setTitle("New Waifu", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 10
         button.titleLabel?.font = .boldSystemFont(ofSize: 30)
         return button
     }()
@@ -26,10 +33,34 @@ final class WaifuViewController: UIViewController {
         super.viewDidLoad()
 
         setUpUI()
+        presenter.setViewDelegate(waifuDelegate: self)
+        presenter.getWaifu()
     }
 
-    private func setUpUI() {
+    @objc func setNewWaifu() {
+        presenter.getWaifu()
+        print("tap")
+    }
 
+    func displayWaifu(waifu: UIImage) {
+        imageView.image = waifu
+    }
+    
+    private func setUpUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        view.addSubview(waifuButton)
+
+        waifuButton.addTarget(self, action: #selector(setNewWaifu), for: .touchUpInside)
+
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
+
+        waifuButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -50).isActive = true
+        waifuButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+        waifuButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:  -50).isActive = true
     }
 
 }
